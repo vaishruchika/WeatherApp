2e3a403a5ca7dae2ba5637324116866689b1d7ac
ruchika <ruchikavaish@mastersindia.co>
2023-09-02 12:41:49 +0530

    changes

--- src/Store/detailsSaga.js
+++ src/Store/detailsSaga.js
@@ -3,7 +3,7 @@ import {GET_WEATHER_DETAILS, REQUEST_FAILED, SET_WEATHER_DETAILS} from './consta
 
 function* getDetails({payload}) {
     try {
-        let data = yield fetch(`http://api.weatherapi.com/v1/forecast.json?key=18d6d64ca62a4b8da1260436233108&q=india/${payload}/`)
+        let data = yield fetch(`https://api.weatherapi.com/v1/forecast.json?key=18d6d64ca62a4b8da1260436233108&q=india/${payload}/`)
         data = yield data.json();
         if (data.error) {
             yield put({type: REQUEST_FAILED, error: data.error})
